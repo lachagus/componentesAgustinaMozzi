38516e4bf87c4991a5f40b69fecf86500128a2bf
lachagus <chagusbigand@gmail.com>
2022-03-20 03:07:14 -0300

    PrimeraEntrega

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ContentsComponent } from './components/contents/contents.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    FooterComponent,
+    NavbarComponent,
+    ContentsComponent
   ],
   imports: [
     BrowserModule,
